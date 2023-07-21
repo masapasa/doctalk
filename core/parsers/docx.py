01363ccbc0c484b467acfd2e0cc52e282cd69b03
masapasa <apyakurel@gmail.com>
2023-07-21 12:29:19 +0200

    reconfigure

--- core/parsers/docx.py
+++
@@ -1,9 +0,0 @@
-from langchain.document_loaders import Docx2txtLoader
-from core.models.files import File
-from core.models.settings import CommonsDep
-
-from .common import process_file
-
-
-def process_docx(commons: CommonsDep, file: File, enable_summarization, brain_id, user_openai_api_key):
-    return process_file(commons, file, Docx2txtLoader, enable_summarization, brain_id, user_openai_api_key)
